0cf66e9801549e2850d74688b55fbd987e9cedce
leon <email.leonai@gmail.com>
2024-11-23 23:13:39 -0500

    Update 网易云Web完整歌单.js
    Modify the support links

--- WebBrowser/网易云Web完整歌单.js
+++ WebBrowser/网易云Web完整歌单.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name        网易云Web完整歌单
 // @name:en     Show the full list of songs in Netease Music Web Player
-// @namespace   https://raw.githubusercontent.com/leonlovecode/MonkeyScripts/refs/heads/main/WebBrowser
+// @namespace   https://github.com/leonlovecode/MonkeyScripts/edit/main/WebBrowser
 // @match       *://music.163.com/*
 // @icon        https://s1.music.126.net/style/favicon.ico
 // @grant       none
-// @version     20241113
+// @version     20241113-V1.0
 // @author      Leon
 // @description Show the full list of songs in Netease Music Web Player. Only works on website music.163.com
-// @homepageURL https://raw.githubusercontent.com/leonlovecode/MonkeyScripts/refs/heads/main/WebBrowser
+// @homepageURL https://github.com/leonlovecode/MonkeyScripts/edit/main/WebBrowser
 // @downloadURL https://raw.githubusercontent.com/leonlovecode/MonkeyScripts/refs/heads/main/WebBrowser/%E7%BD%91%E6%98%93%E4%BA%91Web%E5%AE%8C%E6%95%B4%E6%AD%8C%E5%8D%95.js
 // @updateURL   https://raw.githubusercontent.com/leonlovecode/MonkeyScripts/refs/heads/main/WebBrowser/%E7%BD%91%E6%98%93%E4%BA%91Web%E5%AE%8C%E6%95%B4%E6%AD%8C%E5%8D%95.js
-// @supportURL  https://raw.githubusercontent.com/leonlovecode/MonkeyScripts/refs/heads/main/WebBrowser
+// @supportURL  https://github.com/leonlovecode/MonkeyScripts/edit/main/WebBrowser
 // ==/UserScript==
 
 (function () {
