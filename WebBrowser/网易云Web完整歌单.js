01b0ef15048160a84b7c9be9ce4ce05c7a93319c
leon <email.leonai@gmail.com>
2024-11-23 21:30:21 -0500

    Create 网易云Web完整歌单.js

---
+++ WebBrowser/网易云Web完整歌单.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        网易云Web完整歌单
+// @namespace   Violentmonkey Scripts
+// @match       *://music.163.com/*
+// @icon        https://s1.music.126.net/style/favicon.ico
+// @grant       none
+// @version     20241113
+// @author      Leon
+// @description 2024/11/23 Manually added
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    document.cookie = 'os=pc';
+})();
